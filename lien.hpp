96c0aa7f97388792260a18dfe35d5b54e5869650
Areolame <areolame@gmail.com>
2022-11-05 20:51:25 +0100

    Ajout de classes basiques

---
+++ lien.hpp
@@ -0,0 +1,27 @@
+#ifndef LIEN_HPP
+#define LIEN_HPP
+
+include "noeud.hpp"
+
+class Lien {
+public:
+	Noeud _noeud1, _noeud2;
+	Lien(Noeud* noeud1, Noeud* noeud2) {
+		_noeud1 = noeud1;
+		_noeud2 = noeud2;
+	}
+};
+
+Noeud getNoeud(int number)  const 
+{
+	if (number == 1) return _noeud1;
+	return _noeud2;
+}
+Noeud getNoeud1()  const { return _noeud1; }
+Noeud getNoeud2()  const { return _noeud2; }
+
+void setNoeud1(Noeud* noeud1) { _noeud1 = noeud1; }
+void setNoeud2(Noeud* noeud2) { _noeud2 = noeud2; }
+
+
+#endif
---
+++ noeud.hpp
@@ -0,0 +1,27 @@
+#ifndef NOEUD_HPP
+#define NOEUD_HPP
+
+#include "point.hpp"
+
+class Noeud {
+public:
+	Point _position;
+	Noeud(Point* position) {
+		_position = position;
+	}
+	Noeud(int* x, int* y) {
+		_position = Point(x, y);
+	}
+};
+
+Point getPosition()  const { return _position; }
+int getX()  const { return _position.getX(); }
+int getY()  const { return _position.getY(); }
+
+void setPosition(int* x, int* y) { _position.setX(x); _position.setY(y); }
+void setPosition(Point* point) { _position = point; }
+void setX(int* x) { _position.setX(x); }
+void setY(int* y) { _position.setY(Y); }
+
+
+#endif
